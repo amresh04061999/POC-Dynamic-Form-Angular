9b6f79019c9f0cb895a5ba3f26447b0a5c13a73c
amresh prasad <amresh04061999@gmail.com>
2024-07-02 17:27:32 +0530

    feat:design completed

--- src/app/components/left-aside/left-aside.component.ts
+++ src/app/components/left-aside/left-aside.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-left-aside',
   standalone: true,
-  imports: [CdkAccordionModule,CommonModule],
+  imports: [CdkAccordionModule, CommonModule],
   templateUrl: './left-aside.component.html',
   styleUrl: './left-aside.component.scss'
 })
 export class LeftAsideComponent {
-  items = ['Basic', 'Advanced', 'Layout', 'Data', 'Premium'];
+  // items = ['Basic', 'Advanced', 'Layout', 'Data', 'Premium'];
+  items = [
+    { title: 'Basic', body: '', active: false },
+    { title: 'Advanced', body: '', active: false },
+    { title: 'Layout', body: '', active: false },
+    { title: 'Data', body: '', active: false },
+    { title: 'Premium', body: '', active: false }
+  ];
+
   expandedIndex = 0;
+
+
 }
